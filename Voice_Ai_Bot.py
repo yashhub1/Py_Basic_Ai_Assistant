b29b01cbe16f5382197a7bf8315910547f9fb65a
yashhub1 <surpaithankary19@gmail.com>
2026-04-18 18:13:35 +0530

    Improved Readability

--- Voice_Ai_Bot.py
+++ Voice_Ai_Bot.py
@@ -1,13 +1,11 @@
 import pyttsx3
 from openai import OpenAI
-
 client = OpenAI()
 voice = pyttsx3.init()
 
 while True:
     user_input = input("You: ")
     if user_input.lower() == "exit":
-        # voice.say("Bye Boi")
         # voice.runAndWait()
         print("Chatbot ended.")
         break
@@ -15,11 +13,11 @@ while True:
     voice.runAndWait()
     prompt = (
         "Answer in easy english but in 1 line  only: " + user_input
-    )  # this is prompt engineering
+    )  
     response = client.responses.create(
         model="gpt-4.1-mini", input=prompt, max_output_tokens=50
     )
 
-    print("AI:", response.output_text)
+    print("Assistant:", response.output_text)
     voice.say(response.output_text)
  
