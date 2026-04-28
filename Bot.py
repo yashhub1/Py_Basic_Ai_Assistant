1abd98c22dbcf8ffefd39d53c4b4ec98ba3bc302
Yash Surpaithankar <surpaithankary19@gmail.com>
2026-04-28 15:22:02 +0530

    Added Dockerfile and requirements

--- Bot.py
+++ Bot.py
@@ -1,23 +1,16 @@
-import pyttsx3
 from openai import OpenAI
 client = OpenAI()
-voice = pyttsx3.init()
 
 while True:
     user_input = input("You: ")
     if user_input.lower() == "exit":
-        # voice.runAndWait()
         print("Chatbot ended.")
         break
-    
-    voice.runAndWait()
-    prompt = (
-        "Answer in easy english but in 1 line  only: " + user_input
-    )  
+
+    prompt = "Answer in easy english but in 1 line only: " + user_input
+
     response = client.responses.create(
         model="gpt-4.1-mini", input=prompt, max_output_tokens=50
     )
 
     print("Assistant:", response.output_text)
-    voice.say(response.output_text)
- 
